0d1d73186404a50394554a0d70aa70302e60c5a1
Celina Louissaint <celinalouissaint@gmail.com>
2021-07-25 21:19:00 -0700

    updated resume img and links

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import resume from '../assets/imgs/resumeCelina.jpg'
-
+import resume from '../assets/imgs/resumeJUL.jpg'
 
 function Resume() {
     return (
         <div className="resume">
-            <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1U-KLrBxgqGlgVTAkhSbv3gkgC7i4dE7q/view?usp=sharing"
+            <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/18wpXJMldGKJcn8AEQ-0M7Ir-7g9whIJz/view?usp=sharing"
                 className="btn btn-outline-secondary btn-sm resumeBtn" role="button" aria-pressed="true">Click Here to Download</a>
-            <a href="https://drive.google.com/file/d/1U-KLrBxgqGlgVTAkhSbv3gkgC7i4dE7q/view?usp=sharing" target="_blank" rel="noreferrer" >
+            <a href="https://drive.google.com/file/d/18wpXJMldGKJcn8AEQ-0M7Ir-7g9whIJz/view?usp=sharing" target="_blank" rel="noreferrer" >
                 <img className="resumeImg" src={resume} alt="Celina's Resume"></img>
             </a>
         </div>
