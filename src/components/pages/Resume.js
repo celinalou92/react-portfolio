808641d3554ed851cfb004fafcee477edad002a1
Celina Louissaint <celinalouissaint@gmail.com>
2021-09-15 14:40:06 -0400

    update contact info and resume link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,9 +4,9 @@ import resume from '../assets/imgs/resumeCelina.jpg'
 function Resume() {
     return (
         <div className="resume">
-            <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1O4vwsk1XuoftIl5mU2-5aXaFGt7ICxcz/view?usp=sharing"
+            <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1NbWV02k_4RNbMWh8i86h1iw--T2YozSm/view?usp=sharing"
                 className="btn btn-outline-secondary btn-sm resumeBtn" role="button" aria-pressed="true">Click Here to Download</a>
-            <a href="https://drive.google.com/file/d/1O4vwsk1XuoftIl5mU2-5aXaFGt7ICxcz/view?usp=sharing" target="_blank" rel="noreferrer" >
+            <a href="https://drive.google.com/file/d/1NbWV02k_4RNbMWh8i86h1iw--T2YozSm/view?usp=sharing" target="_blank" rel="noreferrer" >
                 <img className="resumeImg" src={resume} alt="Celina's Resume"></img>
             </a>
         </div>
